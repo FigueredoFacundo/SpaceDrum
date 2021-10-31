4fe31fb939f374c48fcfd8542ad110ced8fb9f7c
Kevin Sanchez <erik18012@gmail.com>
2021-10-31 18:33:10 -0300

    Enemigos más alejados

--- src/Juego/Logica/Partida.java
+++ src/Juego/Logica/Partida.java
@@ -22,16 +22,16 @@ public class Partida {
     RecursosExternos.init();
     Mapa mapa = new Mapa();
     VentanaPrincipal ventana= new VentanaPrincipal(mapa);
-	Enemigo enemigo1 = new Enemigo(new Circulo(new Punto(500, -50), 20), 100, 25, new Angulo(110),
+	Enemigo enemigo1 = new Enemigo(new Circulo(new Punto(1000, -50), 20), 100, 25, new Angulo(110),
 			new Proyectil(new Circulo(new Punto(20, 20), 1), -1,RecursosExternos.laserRed),RecursosExternos.enemigo,2);
 	
-	Enemigo enemigo2 = new Enemigo(new Circulo(new Punto(500, 700), 30), 150, 25, new Angulo(270),
+	Enemigo enemigo2 = new Enemigo(new Circulo(new Punto(1000, 700), 30), 150, 25, new Angulo(270),
 			new Proyectil(new Circulo(new Punto(20, 20), 1), -1,RecursosExternos.laserRed),RecursosExternos.enemigo2,3);
 	
-	Enemigo enemigo3 = new Enemigo(new Circulo(new Punto(500, 600), 35), 40, 25, new Angulo(180),
+	Enemigo enemigo3 = new Enemigo(new Circulo(new Punto(1000, 600), 35), 40, 25, new Angulo(180),
 			new Proyectil(new Circulo(new Punto(20, 20), 1), -1,RecursosExternos.laserRed),RecursosExternos.enemigo3,1);
 	
-	Enemigo jefe = new Enemigo(new Circulo(new Punto(500, 450), 50), 1000, 40, new Angulo(0),
+	Enemigo jefe = new Enemigo(new Circulo(new Punto(1000, 450), 50), 1000, 40, new Angulo(0),
 
 			new Proyectil(new Circulo(new Punto(20, 20), 1), -1,RecursosExternos.laserRed),RecursosExternos.enemigo4,0);
 	ventana.iniciar();
